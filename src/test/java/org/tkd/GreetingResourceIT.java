9f379f5109389d506b4c7c44baaaf889f63cb0b7
quarkusio <no-reply@quarkus.io>
2024-06-17 16:11:40 +0000

    Initial commit

---
+++ src/test/java/org/tkd/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.tkd;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
